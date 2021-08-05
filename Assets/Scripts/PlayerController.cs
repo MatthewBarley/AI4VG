c273e5bd5734a54bb47b3e594c81300ec0a91cf0
MatthewBarley <matteo.malorgio7@gmail.com>
2021-08-06 00:47:15 +0200

    add agent fsm

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+
+public class PlayerController : MonoBehaviour
+{
+	[SerializeField] [Range(0f, 10f)] private float movementSpeed = 5f;
+	[SerializeField] [Range(0f, 360f)] private float rotationSensitivity = 90f;
+
+	private Rigidbody rb;
+
+	void Start()
+	{
+		rb = GetComponent<Rigidbody>();
+	}
+
+	void FixedUpdate()
+	{
+		// gas and brake are converted into a translation forward/backward
+		rb.MovePosition(transform.position
+						 + transform.forward * movementSpeed * (Input.GetAxis("Vertical") * Time.deltaTime));
+		// steering is translated into a rotation
+		rb.MoveRotation(Quaternion.Euler(0.0f, rotationSensitivity * (Input.GetAxis("Horizontal") * Time.deltaTime), 0.0f)
+						* transform.rotation);
+	}
+}
--- Assets/Scripts/StealthyAgent.cs
+++ Assets/Scripts/StealthyAgent.cs
@@ -1,14 +1,70 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
 public class StealthyAgent : MonoBehaviour
 {
+    public GameObject player;
     public GameObject destination;
+
+    [SerializeField] [Range(5f, 15f)] private float range = 10f;
+    private FSM fsm;
+    private DecisionTree decisionTree;
 
     void Start()
     {
+        FSMState moving = new FSMState();
+        moving.enterActions.Add(MoveToDestination);
+
+        FSMState idle = new FSMState();
+        idle.enterActions.Add(Stop);
+
+        FSMTransition t1 = new FSMTransition(playerOutOfRange);
+        FSMTransition t2 = new FSMTransition(playerInRange);
+
+        moving.AddTransition(t1, idle);
+        idle.AddTransition(t2, moving);
+
+        fsm = new FSM(moving);
+
+        StartCoroutine(RunFSM());
+    }
+
+    private void OnValidate()
+    {
+        Transform t = transform.Find("Range");
+        if (t != null)
+        {
+            t.localScale = new Vector3(range / transform.localScale.x, 1f, range / transform.localScale.z) / 5f;
+        }
+    }
+
+    private void MoveToDestination()
+    {
         GetComponent<NavMeshAgent>().destination = destination.transform.position;
+    }
+
+    private void Stop()
+    {
+        GetComponent<NavMeshAgent>().destination = transform.position;
+    }
+
+    private bool playerInRange()
+    {
+        return (player.transform.position - transform.position).magnitude <= range ? true : false;
+    }
+
+    private bool playerOutOfRange()
+    {
+        return !playerInRange();
+    }
+
+    IEnumerator RunFSM()
+    {
+        while (true)
+        {
+            fsm.Update();
+            yield return null;
+        }
     }
 }
