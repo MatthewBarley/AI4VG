3930162e49c0787b0d7ce20b2b6134c002be5359
MatthewBarley <matteo.malorgio7@gmail.com>
2021-08-05 18:55:39 +0200

    add AI library

--- Assets/Scripts/StealthyAgent.cs
+++ Assets/Scripts/StealthyAgent.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class MoveTo : MonoBehaviour
+public class StealthyAgent : MonoBehaviour
 {
     public GameObject destination;
 
